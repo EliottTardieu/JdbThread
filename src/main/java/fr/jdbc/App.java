1a44b087468bde1d3780bee04312f20fecba01d9
eliott <yodamousta@gmail.com>
2021-12-14 16:17:23 +0100

    Order in App.

--- src/main/java/fr/jdbc/App.java
+++ src/main/java/fr/jdbc/App.java
@@ -1,7 +1,15 @@
 package fr.jdbc;
 
 import fr.jdbc.database.*;
+import fr.jdbc.models.Client;
+import fr.jdbc.models.FullAddress;
+import fr.jdbc.models.Order;
+import fr.jdbc.models.Product;
 import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
 
 public class App {
 
@@ -21,6 +29,26 @@ public class App {
     private final SupplyDAO supplyDAO = new SupplyDAO();
 
     private App(){}
+
+    public void createOrder(LinkedList<Product> products, LinkedList<Integer> quantities, int price, String clientName, FullAddress clientAddress) {
+        int clientId;
+        HashMap<String, Object> criterias = new HashMap<>();
+        criterias.put("nom", clientName);
+        criterias.put("adresse", clientAddress.getId());
+        if(this.clientDAO.find(criterias).exist()) {
+            clientId = this.clientDAO.find(criterias).getId();
+        } else {
+            Client client = new Client();
+            client.setName(clientName);
+            client.setFullAddress(clientAddress);
+            clientId = client.getId();
+        }
+        Order order = new Order();
+        order.setProducts(products);
+        order.setQuantityProduct(quantities);
+        order.setPrice(price);
+        order.setClient(this.clientDAO.findById(clientId));
+    }
 
     public static App getInstance(){
         if(instance == null) return new App();
